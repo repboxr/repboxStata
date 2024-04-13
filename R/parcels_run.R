dc1175c6cbb2284f68ecb858217edc3eb04188d8
skranz-auto <xsx50@uni-ulm.de>
2024-04-13 21:23:57 +0000

    Update from server

--- R/parcels_run.R
+++ R/parcels_run.R
@@ -185,3 +185,68 @@ repbox_save_stata_reg_run_parcels = function(project_dir, parcels=list()) {
   invisible(parcels)
 }
 
+
+make_parcel_stata_do_run_info = function(project_dir, parcels = list()) {
+  restore.point("make_parcel_stata_do_run_info")
+  library(repboxDB)
+  parcels = repdb_load_parcels(project_dir, c("stata_file","stata_run_cmd"), parcels=parcels)
+
+  do_df = parcels$stata_file$script_file
+
+  if (NROW(do_df)==0) {
+    parcels$stata_do_run_info = list(stata_do_run_info = repdb_null_to_empty(NULL, "stata_do_run_info"))
+    repboxDB::repdb_save_parcels(parcels["stata_do_run_info"],file.path(project_dir, "repdb"))
+    return(parcels)
+  }
+
+  # dotab contains some information that we have not yet nicely stored
+  # in a repdb table
+  dotab_file = file.path(project_dir, "/repbox/stata/dotab.Rds")
+  dotab = readRDS.or.null(dotab_file)
+  if (is.null(dotab)) {
+    dotab = data.frame(file_path=character(0), timeout=logical(0), runtime=numeric(0),is.included=logical(0), parse.err = logical(0))
+  }
+  #cat(paste0('"', names(dotab),'"', collapse=", "))
+  old_cols = c("num.reg.lines", "parse.err",  "is.included", "does.include", "timeout", "runtime")
+  new_cols = c("num_reg_lines", "has_parse_err",  "is_included", "does_include", "timeout", "runtime")
+  dotab = rename.cols(dotab, old_cols, new_cols)
+
+  dotab$file_path = str.right.of(dotab$file,paste0(dotab$project_dir,"/mod/"))
+  dotab$analyzed = rep(TRUE, NROW(dotab))
+  do_df = left_join(do_df, dotab, by="file_path") %>%
+    mutate(analyzed = na.val(analyzed, FALSE))
+
+  #cmd_df = parcels$stata_cmd$stata_cmd
+  run_df = parcels$stata_run_cmd$stata_run_cmd
+
+  run_info_df = run_df %>%
+    mutate(
+      loads_data = cmd %in% c("use","u","us","import","guse","gzuse","insheet"),
+      has_error = is.true(errcode != 0)
+    ) %>%
+    group_by(file_path) %>%
+    summarize(
+      was_run = TRUE,
+      num_runs = n(),
+      num_runs_ok = sum(!has_error & !missing_data),
+      num_runs_no_dat = sum(missing_data),
+      num_runs_err = sum(has_error & !missing_data),
+      num_load_data = sum(loads_data),
+      num_load_data_err = sum(has_error & loads_data)
+    )
+
+  do_df = do_df %>%
+    left_join(run_info_df, by="file_path") %>%
+    mutate(
+      was_run = na.val(was_run,FALSE),
+      has_parse_err = na.val(has_parse_err, FALSE)
+    )
+
+  do_df = repdb_select_fields(do_df, "stata_do_run_info")
+  parcels$stata_do_run_info = list(stata_do_run_info = do_df)
+  repboxDB::repdb_save_parcels(parcels["stata_do_run_info"],file.path(project_dir, "repdb"))
+
+  parcels
+}
+
+
